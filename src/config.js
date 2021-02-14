ad4e45f81c4f0597c3b2facc667f9bbc38851a2e
Angela Masajo <aabmasajo@gmail.com>
2021-02-14 10:31:41 -0600

    still working on tests

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  API_TOKEN: process.env.API_TOKEN || '8f119b9f-8693-4a27-934e-60d4bc4c1905'
+  API_TOKEN: process.env.API_TOKEN || '8f119b9f-8693-4a27-934e-60d4bc4c1905',
+  DB_URL: process.env.DB_URL || 'postgresql://dunder_mifflin@localhost/bookmarks',
 }
---
+++ test/bookmarks-endpoints.spec.js
@@ -0,0 +1,133 @@
+const { expect } = require('chai')
+const knex = require('knex')
+const supertest = require('supertest')
+const fixtures = require('./bookmarks-fixtures')
+const app = require('../src/app')
+//TODO: remove when updating POST and DELETE
+const store = require('../src/store')
+
+
+describe.only('Bookmarks Endpoints', () => {
+  let bookmarksCopy, db
+
+  before('make knex instance', () => {
+    db = knex({
+      client: 'pg',
+      connection: process.env.TEST_DB_URL,
+    })
+    app.set('db', db)
+  })
+
+  after('disconnect from db', () => db.destroy())
+
+  before('clean the table', () => db('bookmarks').truncate())
+
+  afterEach('cleanup', () => db('bookmarks').truncate())
+
+  // TODO: refactor to use db when updating POST and DELETE
+  beforeEach('copy the bookmarks', () => {
+    // copy the bookmarks so we can restore them after testing
+    bookmarksCopy = store.bookmarks.slice()
+  })
+
+  // TODO: refactor to use db when updating POST and DELETE
+  afterEach('restore the bookmarks', () => {
+    // restore the bookmarks back to original
+    store.bookmarks = bookmarksCopy
+  })
+  
+
+  //---UNAUTHORIZED REQUESTS
+  describe(`Unauthorized requests`, () => {
+    it(`responds with 401 Unauthorized for GET /bookmarks`, () => {
+      return supertest(app)
+        .get('/bookmarks')
+        .expect(401, { error: 'Unauthorized request' })
+    })
+
+    it(`responds with 401 Unauthorized for POST /bookmarks`, () => {
+      return supertest(app)
+        .post('/bookmarks')
+        .send({ title: 'test-title', url: 'http://some.thing.com', rating: 1 })
+        .expect(401, { error: 'Unauthorized request' })
+    })
+
+    it(`responds with 401 Unauthorized for GET /bookmarks/:id`, () => {
+      const secondBookmark = store.bookmarks[1]
+      return supertest(app)
+        .get(`/bookmarks/${secondBookmark.id}`)
+        .expect(401, { error: 'Unauthorized request' })
+    })
+
+    it(`responds with 401 Unauthorized for DELETE /bookmarks/:id`, () => {
+      const aBookmark = store.bookmarks[1]
+      return supertest(app)
+        .delete(`/bookmarks/${aBookmark.id}`)
+        .expect(401, { error: 'Unauthorized request' })
+    })
+  })  
+
+
+  //---ENDPOINT /BOOKMARKS
+  describe(`GET /bookmarks`, () => {
+    context(`Given no bookmarks`, () => {
+      it(`responds with 200 and an empty list`, () => {
+        return supertest(app)
+          .get('/bookmarks')
+          .expect(200,[])
+      })
+    })
+
+    context(`Given there are bookmarks in the database`, () => {
+      const testBookmarks = fixtures.makeBookmarksArray()
+
+      beforeEach('insert bookmarks', () => {
+        return db
+          .into('bookmarks')
+          .insert(testBookmarks)
+      })
+
+      it('gets the bookmarks from the store', () => {
+        return supertest(app)
+          .get('/bookmarks')
+          .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+          .expect(200, testBookmarks)
+      })
+    })
+  })
+
+  //---ENDPOINT /BOOKMARKS/:ID
+  describe('GET /bookmarks/:id', () => {
+    context(`Given no bookmarks`, () => {
+      it(`responds 404 whe bookmark doesn't exist`, () => {
+        return supertest(app)
+          .get(`/bookmarks/123`)
+          .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+          .expect(404, {
+            error: { message: `Bookmark Not Found` }
+          })
+      })
+    })
+
+    context('Given there are bookmarks in the database', () => {
+      const testBookmarks = fixtures.makeBookmarksArray()
+
+      beforeEach('insert bookmarks', () => {
+        return db
+          .into('bookmarks')
+          .insert(testBookmarks)
+      })
+
+      it('responds with 200 and the specified bookmark', () => {
+        const bookmarkId = 2
+        const expectedBookmark = testBookmarks[bookmarkId - 1]
+        return supertest(app)
+          .get(`/bookmarks/${bookmarkId}`)
+          .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+          .expect(200, expectedBookmark)
+      })
+    })
+  })
+
+
+})
